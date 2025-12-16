149cce5ab650b02e4276a2a9db2dbc662ad39e8f
immatin21 <mondalmatin04@gmail.com>
2025-12-16 22:10:08 +0530

    Project section changes

--- Portfolio/client/src/components/Education.jsx
+++ Portfolio/client/src/components/Education.jsx
@@ -12,7 +12,7 @@ const eduDetails = [
       type: "CGPA",
       score: 8.06,
     },
-    invert : 80,
+    invert : "invert-80",
     duration: "2023 - 2027",
     status: "Pursuing",
   },
@@ -25,8 +25,8 @@ const eduDetails = [
       type: "Percentage(%)",
       score: 74.8,
     },
+
     duration: "2023",
-   
   },
   {
     institution: "Kendriya Vidyalaya, Godhra",
@@ -38,14 +38,12 @@ const eduDetails = [
       score: 86.5,
     },
     duration: "2021",
-   
   },
 ];
 
 const Education = () => {
   return (
     <section id="education" className="mx-auto max-w-6xl pt-25 pb-10">
-        
       <div className="flex justify-center items-center flex-col gap-4">
         <h1 className="font-bold text-5xl">
           <GradientText
@@ -66,7 +64,7 @@ const Education = () => {
         {eduDetails.map((item, i) => (
           <div className="relative border-l-2 border-purple-500/30 max-w-4xl pl-10 mx-auto">
             <div className="w-3 h-3 bg-purple-500 rounded-full absolute -left-1.5 "></div>
-<div className="absolute inset-0 bg-cyan-300/10 blur-2xl pointer-events-none slow-glow -z-10"></div>
+            <div className="absolute inset-0 bg-cyan-300/10 blur-2xl pointer-events-none slow-glow -z-10"></div>
 
             {/* Education card */}
             <div className="bg-white/5 px-6 py-4 rounded-xl text-[#D1D5DB] my-10 group transition duration-300 hover:scale-105">
@@ -74,22 +72,23 @@ const Education = () => {
                 {/* Logo + details */}
                 <div className="flex items-center gap-4">
                   <img
-                    className={`w-12 h-12 invert-${item.invert}`}
+                    className={`w-12 h-12 ${item.invert}`}
                     src={item.src}
                     alt={item.alt}
                   />
 
                   <div>
                     <h2 className="font-bold text-lg text-white/90">
-                      {
-                        item.institution
-                      }
+                      {item.institution}
                     </h2>
                     <p className="text-sm font-semibold group-hover:text-purple-300 ">
                       {item.course}
                     </p>
                     <p className="text-xs font-semibold mt-1">
-                      {item.marks.type}: <span className="font-medium text-white">{item.marks.score}</span>
+                      {item.marks.type}:{" "}
+                      <span className="font-medium text-white">
+                        {item.marks.score}
+                      </span>
                     </p>
                   </div>
                 </div>
@@ -101,10 +100,7 @@ const Education = () => {
                     {item.duration}
                   </p>
                   <p className="group-hover:text-purple-400 text-center font-medium ">
-                    {
-                        item.status ? `Status: ${item.status}` : ""
-                    }
-                    
+                    {item.status ? `Status: ${item.status}` : ""}
                   </p>
                 </div>
               </div>
