5ed615ca695d471a86bc163a19d22b45a8aa1786
immatin21 <mondalmatin04@gmail.com>
2025-12-18 16:38:16 +0530

    Favicon issue Resolved

--- Portfolio/client/src/App.jsx
+++ Portfolio/client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import HeroPage from "./components/HeroPage";
 import Loading from "./components/Loading";
